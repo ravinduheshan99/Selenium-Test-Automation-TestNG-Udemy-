525aa51d9cf56f7a36bb6b1a5efdb8e5be4cf788
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-11-04 23:09:15 +0530

    Running testcases in TestNG with out java compiler and Importance of xml file in Testing Configuration

---
+++ src/test/day1.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+	
+	@Test
+	public void Demo() {
+		System.out.println("Hello");
+	}
+	
+	@Test
+	public void SecondTest() {
+		System.out.println("Bye");
+	}
+	//Goto project name 
+	//Right Click->TestNG->Convert to testng
+	
+	
+}
---
+++ src/test/day2.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day2 {
+
+	@Test
+	public void Ploan() {
+		System.out.println("Good");
+	}
+	
+}
