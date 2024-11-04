fa379744a17c4843eff8cc0de96f6d9e3a72d79e
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-11-04 23:28:09 +0530

    Prioritizing the testcases with TestNG

---
+++ src/test/day3.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day3 {
+
+	@Test
+	public void WebLoginCarLoan() {
+		System.out.println("WebLoginCarLoan");
+	}
+	
+	@Test
+	public void MobileLoginCarLoan() {
+		System.out.println("MobileLoginCarLoan");
+	}
+	
+	@Test
+	public void LoginAPICarLoan() {
+		System.out.println("LoginAPICarLoan");
+	}
+}
---
+++ src/test/day4.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day4 {
+
+	@Test
+	public void WebLoginHomeLoan() {
+		System.out.println("WebLoginHomeLoan");
+	}
+	
+	@Test
+	public void MobileLoginHomeLoan() {
+		System.out.println("MobileLoginHomeLoan");
+	}
+	
+	@Test
+	public void LoginAPIHomeLoan() {
+		System.out.println("LoginAPIHomeLoan");
+	}
+}
