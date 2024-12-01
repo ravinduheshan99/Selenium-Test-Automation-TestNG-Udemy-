e6efed946b0cdda37d38b1e95cd122e792f85d57
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-12-02 00:39:06 +0530

    TestNG annotations @BeforeTest and @AfterTest

--- src/test/day2.java
+++ src/test/day2.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class day2 {
@@ -7,6 +8,11 @@ public class day2 {
 	@Test
 	public void Ploan() {
 		System.out.println("Good");
+	}
+	
+	@BeforeTest
+	public void prerequisits() {
+		System.out.println("I will execute first");
 	}
 	
 }
