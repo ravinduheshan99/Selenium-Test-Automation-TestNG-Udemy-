3be365fb043f85eab78cac9097c68cc6bf87579b
ravinduheshan99 <ravinduheshan99@gmail.com>
2024-12-06 23:22:46 +0530

    Data provider annotation-parameterizing

--- src/test/day3.java
+++ src/test/day3.java
@@ -1,6 +1,7 @@
 package test;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
@@ -13,10 +14,11 @@ public class day3 {
 		System.out.println("I will execute before every test method in day 3 class");
 	}
 	
-	@Parameters({"URL"})
+	@Parameters({"URL","APIKey/username"})
 	@Test
-	public void WebLoginCarLoan(String urlName) {
-		System.out.println(urlName);
+	public void WebLoginCarLoan(String urlName,String key) {
+		System.out.println("url is : "+urlName);
+		System.out.println("Key is : "+key);
 		System.out.println("WebLoginCarLoan");
 	}
 	
@@ -43,5 +45,32 @@ public class day3 {
 	@AfterMethod
 	public void aftermethod() {
 		System.out.println("I will execute after every test method in day 3 class");
+	}
+	
+	@Test(dataProvider = "getData")
+	public void dataProviderCarLoan(String username, String password) {
+		System.out.println("username : "+username);
+		System.out.println("password : "+password);
+	}
+	
+	@DataProvider
+	public Object getData() {
+		//1st combination- username password - good credit history = row
+		//2nd - username password - no credit history
+		//3rd - fraudulent credit history
+		
+		Object[][] data = new Object[3][2];
+		
+		//1st set
+		data[0][0] = "firstsetusername";
+		data[0][1] = "firstsetpassword";
+		//2nd set
+		data[1][0] = "secondsetusername";
+		data[1][1] = "secondsetpassword";
+		//3rd set
+		data[2][0] = "thirdsetusername";
+		data[2][1] = "thirdsetpassword";
+		
+		return data;
 	}
 }
